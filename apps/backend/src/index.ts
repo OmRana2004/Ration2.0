81f3cd080e7a701e2f2a839b4ed008c706bd3351
OmRana2004 <omrana8755800322@gmail.com>
2026-09-07 17:18:26 +0530

    setup

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const db = []
+
+
+
+app.get("/login", (req, res) => {
+    
+    
+})
+
+app.listen(3001, () => console.log("port is running on 3001"))
---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return(
+    <div className="h-screen flex bg-black text-white justify-center">
+      hello world
+    </div>
+  )
+}
+
+export default App;
