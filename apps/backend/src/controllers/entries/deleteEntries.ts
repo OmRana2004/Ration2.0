6a8961e4e9f89d2b5474ec0fdd3365236118d7eb
OmRana2004 <omrana8755800322@gmail.com>
2026-09-09 23:30:07 +0530

    delete-api

--- apps/backend/src/controllers/entries/deleteEntries.ts
+++ apps/backend/src/controllers/entries/deleteEntries.ts
@@ -1,6 +1,36 @@
 import { Request, Response } from "express"
 import { prisma } from "../../db/index";
 
-export const deleteEntries = async (req: Request, res: Response) => {
-    res.send("I am delete")
-}
+export const deleteEntries = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const id = req.params.id as string;
+
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                msg: "ENTRY ID IS REQUIRED"
+            });
+        }
+
+        await prisma.entry.delete({
+            where: {
+                id,
+            },
+        });
+
+        return res.status(200).json({
+            success: true,
+            msg: "Entry deleted successfully",
+        });
+    } catch (error) {
+        console.log("DELETE ENTRY ERROR:",error);
+
+        return res.status(500).json({
+            success: false,
+            msg: "Internal Server Error",
+        });
+    }
+};
