9b0e362d98abb2d8064161cfeea4c0810284d41c
OmRana2004 <omrana8755800322@gmail.com>
2026-09-09 22:55:36 +0530

    get-route

--- apps/backend/src/controllers/entries/getEntries.ts
+++ apps/backend/src/controllers/entries/getEntries.ts
@@ -1,6 +1,27 @@
 import { Request, Response } from "express"
 import { prisma } from "../../db/index";
 
-export const getEntries = async (req: Request, res: Response) => {
-    res.send("I am get")
-}
+export const getEntries = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const entries = await prisma.entry.findMany({
+            orderBy:{
+                createdAt: "desc"
+            },
+        });
+         
+        return res.status(200).json({
+            susuccess: true,
+            data: entries,
+        });
+    } catch(error) {
+        console.log("GET ENTRITS ERROR:", error);
+
+        return res.status(500).json({
+            success: false,
+            msg: "Internal Server Error"
+        });
+    }
+};
