e9f778b639610fd5f62e5d92789606c3d91cbf2c
OmRana2004 <omrana8755800322@gmail.com>
2026-09-09 23:43:06 +0530

    frontend_setup

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return(
+    <div className="h-screen flex bg-black text-amber-50 justify-center items-center font-extrabold">
+      SITA_RAM
+    </div>
+  )
+}
+export default App;
